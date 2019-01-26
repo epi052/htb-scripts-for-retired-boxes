701f0f4ea1c9ff5c3c16db4218dcd1044c03a531
epi <epibar052@gmail.com>
2019-01-26 14:46:26 +0000

    Update generate-node-red-url.sh

--- reddish/generate-node-red-url.sh
+++ reddish/generate-node-red-url.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-curl -s -X POST 10.10.10.94:1880 | echo http://10.10.10.94:1880/red/$(jq .id | sed 's/"//g')
-
+curl -s -X POST 10.10.10.94:1880 | echo http://10.10.10.94:1880/red/$(jq --raw-output .id)
